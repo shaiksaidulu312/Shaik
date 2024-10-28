be770e7ef24685ff2c2c16ce17f5d11b2aab9440
shaik saidulu <shaiksaidulu312@gmail.com>
2024-10-28 15:16:32 +0530

    updated

--- Test.java
+++ Test.java
@@ -3,7 +3,6 @@ public class Test {
        public static void main(String... args){
           Scanner sc=new Scanner(System.in);
              System.out.println("Enter User Name:");
-              System.out.println("hello");
               sString userName=sc.next();
              System.out.println("Enter password:");
               String password=sc.next();
